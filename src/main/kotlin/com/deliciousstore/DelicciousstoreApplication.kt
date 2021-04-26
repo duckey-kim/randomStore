56a9725ea54105510db40e4ecb7c4de2acac9466
duckey <duckey.kdh@gmail.com>
2021-04-26 16:39:21 +0900

    Rebase first commit

---
+++ src/main/kotlin/com/deliciousstore/DelicciousstoreApplication.kt
@@ -0,0 +1,11 @@
+package com.deliciousstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DelicciousstoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<DelicciousstoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/deliciousstore/DelicciousstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.deliciousstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DelicciousstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
